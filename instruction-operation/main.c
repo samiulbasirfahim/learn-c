d028ed4c43522888127bc6144d4a64b92b86c6eb
Samiul Basir Fahim <samiulbasirfahim360@gmail.com>
2022-09-21 01:59:46 +0600

    instruction and operation done

--- instruction-operation/main.c
+++ instruction-operation/main.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
+#include <math.h>
 // instructions and operators
 
+// main funcion
 int main()
 {
-	int a, b, c;
+	double a, b, c;
 	a = b = c = 4;
 
-	printf("value of %d, value of %d, value of %d", a, b, c);
+	int e = 4, f = 5, g;
+	g = 1;
+	// printf("value of %d, value of %d, value of %d", a, b, c);
+
+	/* value iwll be assigned last variable y, not in x */
+	int x, y = a + g;
+	/* value will be assigned in first vairable, j will declare but its value isnt assign */
+	int l = 2, j;
+	/* 3 to the power 2 */
+	int p = pow(3, 2);
+
+	printf("value of e is %d, value of f is %d and value of g is %d", e, f, g);
+
+	printf("\n p is %d", p);
 
 	return 0;
 }
+
+/*
+	first 8,/,%  then +,-
+	left to right
+	bracate first
+
+	@same like math
+
+ */
